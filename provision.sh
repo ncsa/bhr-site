50a3cea7a3c3292c865b7a08c840ece27479d6e6
Justin Azoff <jazoff@illinois.edu>
2014-09-08 16:41:44 -0400

    fix provisioning

--- provision.sh
+++ provision.sh
@@ -1,10 +1,17 @@
 #!/bin/sh
 
 perl -pi -e "s/br/us/" /etc/apt/sources.list
+export DEBIAN_FRONTEND=noninteractive
 
 apt-get update
 apt-get upgrade -y
 
-apt-get install -y postgresql python-psycopg2 python-dev
+apt-get install -y postgresql python-psycopg2 python-dev python-pip python-virtualenv
 
 su postgres -c 'createdb vagrant'
+su postgres -c 'createuser -DRS vagrant'
+
+if [ ! -e env ] ; then
+    su vagrant -c 'virtualenv ~/env'
+fi
+su vagrant -c '~/env/bin/pip install -r /vagrant/requirements.txt'
