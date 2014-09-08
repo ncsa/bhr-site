23c785a28ee24a2da134bfeeda130b8401f0ddda
Justin Azoff <jazoff@illinois.edu>
2014-09-08 17:53:48 -0400

    fix whitelisting, get the skip_whitelist to work

--- bhr/forms.py
+++ bhr/forms.py
@@ -3,8 +3,13 @@ from django.forms import ModelForm
 from bhr.models import is_whitelisted
 
 class BlockEntryForm(ModelForm):
-    def clean_cidr(self):
-        cidr = self.cleaned_data['cidr']
-        item = is_whitelisted(cidr)
-        if item:
-            raise forms.ValidationError("whitelisted: %s: %s" % (item.who, item.why))
+
+    def clean(self):
+        cleaned_data = super(BlockEntryForm, self).clean()
+        cidr = self.cleaned_data.get('cidr')
+        skip_whitelist = self.cleaned_data.get('skip_whitelist')
+        if cidr and not skip_whitelist:
+            item = is_whitelisted(cidr)
+            if item:
+                raise forms.ValidationError("whitelisted: %s: %s" % (item.who, item.why))
+        return cleaned_data
