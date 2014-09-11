8953706f4b7279151c9c3a1ed09af38f3009d5d9
Justin Azoff <jazoff@illinois.edu>
2014-09-11 13:39:12 -0400

    add some nice things to admin page
    the query stuff probably should be split out somewhere

--- bhr/admin.py
+++ bhr/admin.py
@@ -1,8 +1,27 @@
 from django.contrib import admin
 
 # Register your models here.
-from bhr.models import WhitelistEntry, Block
+from bhr.models import WhitelistEntry, Block, BlockEntry
 from bhr.forms import BlockForm
+
+def force_unblock(modeladmin, request, queryset):
+    queryset.update(forced_unblock=True)
+force_unblock.short_description = "Force Unblock"
+
+class BlockStatusListFilter(admin.SimpleListFilter):
+    title = "status"
+    parameter_name = "status"
+
+    def lookups(self, request, model_admin):
+        return (
+            ('current', 'currently blocked'),
+        )
+
+    def queryset(self, request, queryset):
+        if self.value() == "current":
+            return queryset.filter(
+                id__in = BlockEntry.objects.distinct('block_id').filter(removed__isnull=True).values_list('block_id', flat=True))
+
 
 class AutoWho(admin.ModelAdmin):
     def save_model(self, request, obj, form, change):
@@ -10,11 +29,11 @@ class AutoWho(admin.ModelAdmin):
             obj.who = request.user
         obj.save()
 
-
 class BlockAdmin(AutoWho):
     date_hierarchy = 'added'
-    list_filter = ('who__username', 'source', 'flag')
+    list_filter = ('who__username', 'source', 'flag', BlockStatusListFilter)
     list_display = ('cidr', 'who', 'source')
+    actions = [force_unblock]
 
     form = BlockForm
 
