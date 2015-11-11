f2fcac79659e7680d3abe21e45aa9b4fabe3bd77
Justin Azoff <jazoff@illinois.edu>
2015-11-11 09:28:22 -0500

    Redirect requests for / to /bhr

--- bhr_site/urls.py
+++ bhr_site/urls.py
@@ -1,5 +1,6 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
     # Examples:
@@ -10,4 +11,6 @@ urlpatterns = patterns('',
     url(r'^bhr/', include('bhr.urls')),
     url(r'^accounts/logout/$', 'django.contrib.auth.views.logout'),
     url(r'^accounts/login/$', 'django.contrib.auth.views.login', {'template_name': 'admin/login.html'}),
+
+    url(r'^$', RedirectView.as_view(url='/bhr', permanent=False), name='siteroot'),
 )
