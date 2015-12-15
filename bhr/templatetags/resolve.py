5ed006297313b5f7d6ae2760abf54290181b267c
Justin Azoff <jazoff@illinois.edu>
2015-12-15 13:18:58 -0500

    commit missing files from last change

---
+++ bhr/templatetags/resolve.py
@@ -0,0 +1,13 @@
+from bhr.util import resolve 
+
+from django import template
+from django.template.defaultfilters import stringfilter
+
+
+register = template.Library()
+
+@register.filter(name="resolve")
+@stringfilter
+def resolve_tag(value):
+    ip = value.split("/")[0]
+    return resolve(ip)
