fc870a014a48ed097e538eb28492eb19ffb7eed0
Justin Azoff <jazoff@illinois.edu>
2016-05-10 19:14:28 -0400

    ensure STATIC_ROOT is set to something

--- bhr_site/settings.py
+++ bhr_site/settings.py
@@ -96,6 +96,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 LOGGING = {
     'version': 1,
