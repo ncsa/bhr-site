ncsa/bhr-site

commit 57c3d3b54b50484d366d4546c3673f1a6e4d05c6

Speed up the block_queue query.
Before Time: 526.680 ms
After  Time:  28.875 ms

bhr/models.py
@@ -300,7 +300,7 @@ class BHRDB(object):
         return list(Block.objects.raw("""
             SELECT b.id as pk, * from bhr_block b
             LEFT JOIN bhr_blockentry be
-            ON b.id=be.block_id AND be.ident = %s
+            ON b.id=be.block_id AND be.ident = %s AND be.removed IS NULL
             WHERE
                 (b.unblock_at IS NULL OR
                  b.unblock_at > %s)

commit f205b93eac0f1821db4992d8b0c2838040ee2051

problem: incorrect block queue query can cause SQL errors.
This fixes a bug introduced in 57c3d3b54b50484d366d4546c3673f1a6e4d05c6
that is also related to the fix in 729c6605ad0d2558e2a7a9bcd5476f50a258a53f
that attempts to fix problems caused by overlapping blocks.

The over aggressive 'be.removed is NULL' causes the 'fixed' blockentry
records to not match causing the block queue to return entries that are
already blocked.

This change hurts performance a bit, but due to the recent changes in
the API, it will only affect the first block queue query when a
bhr-client starts up.

The work outlined in #2 about clearing old bhr_blockentries should help
with this.

bhr/models.py
@@ -320,7 +320,7 @@ class BHRDB(object):
         return Block.objects.raw("""
             SELECT b.id as pk, * from bhr_block b
             LEFT JOIN bhr_blockentry be
-            ON b.id=be.block_id AND be.ident = %s AND be.removed IS NULL
+            ON b.id=be.block_id AND be.ident = %s
             WHERE
                 b.added >= %s
             AND
@@ -329,7 +329,7 @@ class BHRDB(object):
             AND
                 b.forced_unblock is false
             AND
-                be.added IS NULL
+                be.added IS NULL AND be.removed is NULL
             ORDER BY
                 b.added ASC
             LIMIT %s """,