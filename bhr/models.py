ncsa/bhr-site

commit bd82a1675fce5b72c1c011d508774ea10b22506a

Skip whitelist checks during a force unblock.
When a block record is being force unblocked and a save() is called on
the Block object, skip any whitelist checks.

This fixes the problem caused by

 * blocking an ip
 * whitelisting that ip
 * trying to unblock that ip

During the final save() the whitelist check was preventing the
Block object from being saved.  If unblock_now is set, any whitelist
checks can be skipped.

Whitelist checks really only need to be ran on a save if block.cidr
changed, but that will require a more involved code change.

This fixes #3

bhr/models.py
@@ -137,7 +137,7 @@ class Block(models.Model):
     expired = ExpiredBlockManager()
 
     def save(self, *args, **kwargs):
-        if not self.skip_whitelist:
+        if not self.skip_whitelist and not self.unblock_now:
             wle = is_whitelisted(self.cidr)
             if wle:
                 raise WhitelistError(wle.why)

bhr/tests.py
@@ -246,6 +246,11 @@ class DBTests(TestCase):
         self.assertEqual(bool(is_whitelisted("141.142.4.0/24")), True)
         self.assertEqual(bool(is_whitelisted("141.0.0.0/8")), True)
 
+    def test_block_then_whitelist_then_unblock_works(self):
+        b1 = self.db.add_block('1.2.3.4', self.user, 'other', 'testing')
+        WhitelistEntry(who=self.user, why='test', cidr='1.2.3.0/24').save()
+        self.db.unblock_now('1.2.3.4', self.user, 'testing')
+
     def test_prefixlen_too_small(self):
         self.assertEqual(bool(is_prefixlen_too_small("1.2.3.4")), False)
         self.assertEqual(bool(is_prefixlen_too_small("1.2.3.4/24")), False)

commit 1f315167fb84fb32ab608a5c644d9062305629fe

problem: fix old logic bug
Fix bug introduced back in bd82a167 that made this if statement always
False.  unblock_now is the function, forced_unblock is the boolean

bhr/models.py
@@ -140,7 +140,7 @@ class Block(models.Model):
     expired = ExpiredBlockManager()
 
     def save(self, *args, **kwargs):
-        if not self.skip_whitelist and not self.unblock_now:
+        if self.skip_whitelist is False and self.forced_unblock is False:
             wle = is_whitelisted(self.cidr)
             if wle:
                 raise WhitelistError(wle.why)