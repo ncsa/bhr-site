90495e5c3046b42703371e67770077ceeab06231
Justin Azoff <jazoff@illinois.edu>
2017-08-07 21:28:34 -0400

    problem: unblock query does not use index
    add an order by so that the unblock queue query hits the index

--- bhr/models.py
+++ bhr/models.py
@@ -92,7 +92,7 @@ class PendingRemovalBlockManager(models.Manager):
     def get_queryset(self):
         return super(PendingRemovalBlockManager, self).get_queryset().filter(
             id__in = BlockEntry.objects.distinct('block_id').filter(removed__isnull=True, unblock_at__lte=timezone.now()).values_list('block_id', flat=True)
-        )
+        ).order_by('unblock_at')
 
 class ExpiredBlockManager(models.Manager):
     def get_queryset(self):
